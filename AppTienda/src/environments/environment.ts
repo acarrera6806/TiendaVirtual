e1f385cba4d621afc2367983894d897a275ebf6d
Alejandro Carrera <63697080+acarrera6806@users.noreply.github.com>
2022-12-03 18:13:13 -0500

    Update environment.ts

--- AppTienda/src/environments/environment.ts
+++ AppTienda/src/environments/environment.ts
@@ -2,14 +2,14 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-    apiKey: "AIzaSyBiniRAjfok3yAUBgyoy-ATFolT9sPY-yE",
-    authDomain: "apptienda-130f3.firebaseapp.com",
-    projectId: "apptienda-130f3",
-    storageBucket: "apptienda-130f3.appspot.com",
+    apiKey: "api key",
+    authDomain: "dominio",
+    projectId: "proyecto",
+    storageBucket: "bucket",
     messagingSenderId: "818210694167",
-    appId: "1:818210694167:web:d5eb561224ad303674e852",
+    appId: "id app",
     measurementId: "G-GY4Q2F9D6F"
   },
-  clientId: 'AaTKw3YjCMAQTlcgYMIv7abKRjBx_uUoV00ET7LzTXvqFjblziBKCbHTqbzUqfB0RECGiZ8E3KhjLm_E'
+  clientId: 'id'
 };
 
